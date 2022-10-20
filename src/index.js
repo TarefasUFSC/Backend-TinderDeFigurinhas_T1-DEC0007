297830933c065a5b8d9381efaca05220aa3045c9
CodeWracker <dev.rodrigofs@gmail.com>
2022-10-19 22:58:42 -0300

    fiz a rota de cadastro e de login

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ const app = express();
 const WebSocket = require("ws");
 
 const mongoose = require('mongoose');
+const UserController = require("./controllers/UserController");
 // get env variables
 require('dotenv').config();
 
-mongoose.connect("mongodb+srv://gourmet:"+process.env.MONGODB_PSW+"@clusterdec0007.itfze4v.mongodb.net/DEC0007_T1?retryWrites=true&w=majority", {useMongoClient: true })
+mongoose.connect("mongodb+srv://gourmet:"+process.env.MONGODB_PSW+"@clusterdec0007.itfze4v.mongodb.net/DEC0007_T1?retryWrites=true&w=majority", {useNewUrlParser: true })
 const database = mongoose.connection;
 database.on('error',(e)=>{console.log(e);})
 database.once('connected',()=>{console.log("Db conectado");})
@@ -25,9 +26,20 @@ const wss = new WebSocket.Server(
 
 wss.on("connection", function connection(ws) {
   console.log("New connection");
-  ws.on("message", function incoming(message) {
+  ws.on("message", async function incoming(message) {
     var msg = JSON.parse(message);
     console.log(msg);
+    switch (msg.type) {
+      case "login":
+        console.log("Login");
+        const rsp = await UserController.login(msg.data);
+        if(rsp.error){
+          ws.send(JSON.stringify({type:"login",data:{error:rsp.error}}));
+        }else{
+          ws.send(JSON.stringify({type:"login",data:{user:rsp}}));
+        }
+        break;
+    }
   });
 
   ws.send(JSON.stringify({msg:"Roi, client-kun"}));
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,15 @@ const routes = express.Router();
 
 
 const FiguraController = require("./controllers/FiguraController");
+const UserController = require("./controllers/UserController");
 
+
+
+// Rotas de Usuário	
+routes.post('/signup', UserController.signup);
+
+// Rotas de Figuras
 routes.get("/figura/:id", FiguraController.getFigById);
+
 
 module.exports = routes;
