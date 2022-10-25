144d9f46d872ee88fe2c229903cf91558c42dce6
CodeWracker <dev.rodrigofs@gmail.com>
2022-10-25 19:17:14 -0300

    melhorei o codigo para remov er figurinhas, mas não testei com a remoção do match

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,6 +6,102 @@ const Figure = require('../db/models/figure');
 const Match = require('../db/models/match');
 const saltedMd5 = require('salted-md5');
 
+
+async function checkIfFigureHasACopy(figureId, id_user) {
+    const fig_in_user = await User.findOne(
+        {
+            id_user: id_user,
+            repeated_figs: {
+                $elemMatch: {
+                    id_figure: figureId
+                }
+            }
+        });
+    if (fig_in_user) {
+        return true;
+    }
+    return false;
+
+}
+async function setFiguresFreeFromPromisse(user, match_figures_user) {
+    for (let i = 0; i < match_figures_user.length; i++) {
+        const fig = match_figures_user[i];
+        // set the promissed to false to figure with id_figure == fig.id_figure
+        user.repeated_figs = user.repeated_figs.map((fig) => {
+            if (fig.id_figure == fig.id_figure) {
+                fig.is_promissed = false;
+            }
+            return fig;
+        });
+        await user.save();
+    }
+    return user;
+}
+async function deleteMatch(match_id) {
+    const match = await Match.findOne({ id_match: match_id });
+    // let all figures in the match know that the match was deleted setting the promissed to false
+    const id_user_1 = match.id_user_1;
+    const id_user_2 = match.id_user_2;
+    const user_1 = await User.findOne({ id_user: id_user_1 });
+    const user_2 = await User.findOne({ id_user: id_user_2 });
+    user_1 = await setFiguresFreeFromPromisse(user_1, match.figures.user_1);
+    user_2 = await setFiguresFreeFromPromisse(user_2, match.figures.user_2);
+    // delete the match entry in the database
+    await Match.deleteOne({ id_match: match_id });
+
+
+}
+async function deleteRepeatedFigure(id_figure, id_user) {
+    //------------------
+    // remotion of repeated figure
+    // check if the there is at least one copy of this figure in repeated_figs that is not promissed
+    // if there is, remove it from repeated_figs
+    // if not, delete the match that this figure and then remove it from repeated_figs
+    //------------------
+    // delete match
+    // let all figures in the match know that the match was deleted setting the promissed to false
+    // delete the match entry in the database
+
+    const user = await User.findOne({ id_user: id_user });
+
+    // check if the there is at least one copy of this figure in repeated_figs that is not promissed
+    const not_promissed_figs = user.repeated_figs.filter((fig) => fig.id_figure == id_figure && fig.is_promissed == false);
+    if (not_promissed_figs.length > 0) {
+        // if there is, remove it from repeated_figs
+        const delete_prom_id = not_promissed_figs[0]._id;
+        user.repeated_figs = user.repeated_figs.filter((fig) => fig._id != delete_prom_id);
+        await user.save(); // isso aqui funciona????????????????????
+    }
+    else {
+        // if not, delete the match that this figure and then remove it from repeated_figs
+        const match = await Match.findOne({
+            $or: [
+                {
+                    id_user_1: id_user,
+                    figures: {
+                        $elemMatch: {
+                            id_figure: id_figure
+                        }
+                    }
+                },
+                {
+                    id_user_2: id_user,
+                    figures: {
+                        $elemMatch: {
+                            id_figure: id_figure
+                        }
+                    }
+                }
+            ]
+        });
+        if (match) {
+            await deleteMatch(match.id_match);
+        }
+
+    }
+
+}
+
 module.exports = {
     async login(data) {
         //console.log(data);
@@ -93,10 +189,14 @@ module.exports = {
             return response.status(401).json({ error: "You can't add and remove figures at the same time" });
         }
         console.log("c");
-        if(!new_figures && !remotions){
+        if (!new_figures && !remotions) {
             return response.status(401).json({ error: "You need to add or remove figures" });
         }
         console.log("d");
+
+
+
+
         if (new_figures) {
             // add new figures
             console.log("adding new figs");
@@ -106,165 +206,113 @@ module.exports = {
                     return response.status(401).json({ error: "Figure not found" });
                 }
                 const fig_in_user = await User.findOne(
-                    { 
-                        id_user: id_user, 
-                        unique_figs: { 
-                            $elemMatch: { 
-                                id_figure: new_figures[i] 
-                            } 
+                    {
+                        id_user: id_user,
+                        unique_figs: {
+                            $elemMatch: {
+                                id_figure: new_figures[i]
+                            }
                         }
                     });
                 if (fig_in_user) {
                     // figure already in user
                     const user = await User.findOneAndUpdate(
-                        { id_user: id_user }, 
-                        { $push: { 
-                            repeated_figs: { 
-                                id_figure: new_figures[i], 
-                                is_promissed: false 
-                                } 
-                            } 
+                        { id_user: id_user },
+                        {
+                            $push: {
+                                repeated_figs: {
+                                    id_figure: new_figures[i],
+                                    is_promissed: false
+                                }
+                            }
                         });
                 } else {
                     const user = await User.findOneAndUpdate(
-                        {id_user: id_user}, 
-                        { 
-                            $push: { 
-                                unique_figs: { 
-                                    id_figure: new_figures[i] 
-                                } 
-                            } 
+                        { id_user: id_user },
+                        {
+                            $push: {
+                                unique_figs: {
+                                    id_figure: new_figures[i]
+                                }
+                            }
                         });
-                }     
-                return response.status(200).json({ message: "Figures added to user" });       
-        }
-        
+                }
+            }
 
-    }
-    console.log(remotions);
-        if(remotions.rem_unique){ 
+
+            return response.status(200).json({ message: "Figures added to user" });
+
+
+        }
+        console.log(remotions);
+        if (remotions.rem_unique) {
             // remove figures
             // remotions = {unique_figs, repeated_figs}
-            const {rem_unique} = remotions;
-            if(rem_unique.length > 0){
-                //removes from uniques and take one from repeated if exists and put in uniques, deleting every match that is found with that specific repeated
-                for (let i = 0; i < rem_unique.length; i++) {
-                    const fig = await Figure.findOne({ id_figure: rem_unique[i] });
-                    if (!fig) {
-                        return response.status(401).json({ error: "Figure not found" });
-                    }
-                    const fig_in_user = await User.findOne(
-                        { 
-                            id_user: id_user, 
-                            unique_figs: { 
-                                $elemMatch: { 
-                                    id_figure: rem_unique[i] 
-                                } 
-                            }
-                        });
-                    if (!fig_in_user) {
-                        // figure not in user
-                        return response.status(401).json({ error: "Figure not in user" });
-                    }
-                    const repeated_fig = await User.findOne(
-                        { 
-                            id_user: id_user, 
-                            repeated_figs: { 
-                                $elemMatch: { 
-                                    id_figure: rem_unique[i]
-                                } 
-                            }
-                        });
-                        if(!repeated_fig){
-                            // no repeated figs
-                            return response.status(404).json({ error: "No repeated figures with id " + rem_unique[i] });
-                        }
-                        //console.log(repeated_fig.repeated_figs);
-                        //find every repeated fig with id_figure
-                        const figs = repeated_fig.repeated_figs.filter(fig => fig.id_figure == rem_unique[i]);
-                        console.log(figs);
-                        const prom_figs = figs.filter(fig => fig.is_promissed == true);
-                        console.log(prom_figs);
-                        if(figs.length>prom_figs.length){
-                            // remove repeated and add to uniques
-                            const user = await User.findOneAndUpdate(
-                                { id_user: id_user }, 
-                                { $pull: { 
-                                    repeated_figs: { 
-                                        id_figure: rem_unique[i],
-                                        is_promissed: false 
-                                    } 
-                                } 
-                            });
-                        }
-                        else{
-                            if(prom_figs.length>0){
-                                console.log("promissed");
-                                // remove repeated and add to uniques
-                                const user = await User.findOneAndUpdate(
-                                    { id_user: id_user }, 
-                                    { $pull: { 
-                                        repeated_figs: { 
-                                            id_figure: rem_unique[i],
-                                            is_promissed: true 
-                                        } 
-                                    } 
-                                });
-                                //get all match of this user
-                                const match = await Match.find({
-                                    $or: [
-                                        { id_user1: id_user },
-                                        { id_user2: id_user }
-                                    ]
-                                });
-                                console.log(match);
-                                console.log( match.length);
-                                for(let j = 0; j < match.length; j++){
-                                    if(match[j].id_user_1 == id_user){
-                                        //remove from user1
-                                        console.log(match[j].figures.user_1.length);
-                                        for(let k = 0;k<match[j].figures.user_1.length;k++){
-                                            console.log(match[j].figures.user_1[k].id_figure == rem_unique[i]);
-                                            if(match[j].figures.user_1[k].id_figure == rem_unique[i]){
-                                                await Match.deleteOne({  id_match: match[j].id_match  });
-                                                
-                                                console.log("removed from user 1");
-                                            }
-                                        }
-                                    }
-                                    else{
-                                        for(let k = 0;k<match[j].user2_figs.length;k++){
-                                            if(match[j].user1_figs[k].id_figure == rem_unique[i]){
-                                                await Match.deleteOne({ id_match: match[j].id_match  });
-                                                
-                                            }
-                                        }
-                                    }
-                                }
+            const { rem_unique } = remotions;
 
-                                
-                            }
-                            else{
-                                return response.status(404).json({ error: "Figure not in user" });
+
+
+
+            // check if there is a copy of that unique figure in repeated_figs
+
+            // if theres a copy, remove it from repeated_figs
+            // if not, remove it from unique_figs
+
+            //------------------
+            // remotion of repeated figure
+            // check if the there is at least one copy of this figure in repeated_figs that is not promissed
+            // if there is, remove it from repeated_figs
+            // if not, delete the match that this figure and then remove it from repeated_figs
+            //------------------
+            // delete match
+            // let all figures in the match know that the match was deleted setting the promissed to false
+            // delete the match entry in the database
+
+
+            //removes from uniques and take one from repeated if exists and put in uniques, deleting every match that is found with that specific repeated
+            for (let i = 0; i < rem_unique.length; i++) {
+                const fig_in_user = await User.findOne(
+                    {
+                        id_user: id_user,
+                        unique_figs: {
+                            $elemMatch: {
+                                id_figure: rem_unique[i]
                             }
                         }
-                    /*
-                    if (repeated_fig) {
-                        
-                    }else{
-                        // remove unique
-                        const user = await User.findOneAndUpdate(
-                            { id_user: id_user }, 
-                            { $pull: { 
-                                unique_figs: { 
-                                    id_figure: rem_unique[i] 
-                                } 
-                            } 
-                        });
-                    } */
+                    });
+                if (!fig_in_user) {
+                    // figure not in user
+                    return response.status(401).json({ error: "Figure not in user" });
                 }
+                const has_copy = await checkIfFigureHasACopy(rem_unique[i], id_user);
+                if (has_copy) {
+                    deleteRepeatedFigure(rem_unique[i], id_user);
+
+                } else {
+                    // there is no copy of this figure in repeated_figs than shall be deleted from unique
+                    const user = await User.findOneAndUpdate(
+                        { id_user: id_user },
+                        {
+                            $pull: {
+                                unique_figs: {
+                                    id_figure: rem_unique[i]
+                                }
+                            }
+                        });
+                }
+
+            }
+
+        }
+        if (remotions.rem_repeated) {
+            // remove figures
+            // remotions = {unique_figs, repeated_figs}
+            const { rem_repeated } = remotions;
+            console.log("removing repeated");
+            for (let i = 0; i < rem_repeated.length; i++) {
+                deleteRepeatedFigure(rem_repeated[i], id_user);
             }
         }
         return response.status(200).json({ message: "Operation succeeded" });
-},
+    },
 };
